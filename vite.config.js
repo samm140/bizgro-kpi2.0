188ca619c3c80c47ea189c0e4e67d90a34bcd757
samm140 <sam@bizgropartners.com>
2025-09-11 02:48:28 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-// File: vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Important: Set the base to your repository name for GitHub Pages
   base: '/bizgro-kpi2.0/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
 })
